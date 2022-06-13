5814ed52eeb363bde910e4ba268abdf9e93ebaba
headhunterjz <headhunterjz@gmail.com>
2022-06-13 23:41:47 +0300

    Добавьте файлы проекта.

---
+++ Full1v3/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace Full1v3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(41, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(700, 700);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::Full1v3.Properties.Resources.Rblock;
+            this.button1.Location = new System.Drawing.Point(825, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(54, 45);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(825, 289);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(816, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Удаление";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(816, 388);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(816, 437);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(789, 502);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Номер итерации";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(789, 539);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Текущая итерация";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(789, 572);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Лучший проход";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(922, 502);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(925, 538);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(925, 572);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "0";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(792, 617);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(270, 45);
+            this.trackBar1.TabIndex = 12;
+            this.trackBar1.Value = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1090, 779);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DE";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Full1v3/Form1.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Full1v3
+{
+    public partial class Form1 : Form
+    {
+        int iteration = 1;
+        int iteration_time =0;
+        int best_result = 0;
+        int menu = 0;//добавление объектов
+        int[,] road = { 
+            {0,0,0,0,2,1,0,0,0,0,0,0,0,0,2,7,0,0,0,0 },
+            {0,0,0,0,2,1,0,0,0,0,0,0,0,0,2,1,0,0,0,0 },
+            {0,0,0,0,5,5,4,4,5,5,4,4,4,4,5,5,4,4,5,5 },
+            {0,0,0,0,5,5,3,3,5,5,3,3,3,3,5,5,3,3,5,5 },
+            {0,0,0,0,0,0,0,0,2,1,0,0,0,0,2,1,0,0,2,1 },
+            {0,0,0,0,0,0,0,0,2,1,0,0,0,0,2,1,0,0,2,1 },
+            {0,0,5,5,4,4,4,4,5,5,4,4,4,4,5,5,4,4,5,5 },
+            {0,0,5,5,3,3,3,3,5,5,3,3,3,3,5,5,3,3,5,5 },
+            {0,0,2,1,0,0,0,0,2,1,0,0,0,0,2,1,0,0,2,1 },
+            {0,0,2,1,0,0,0,0,2,1,0,0,0,0,2,1,0,0,2,1 },
+            {0,0,2,1,0,0,0,0,2,1,0,0,0,0,2,1,0,0,2,1 },
+            {0,0,5,5,4,4,4,4,5,5,4,4,4,4,5,5,4,4,5,5 },
+            {0,0,5,5,3,3,3,3,5,5,3,3,3,3,5,5,3,3,5,5 },
+            {0,0,2,1,0,0,0,0,2,1,0,0,0,0,2,1,0,0,0,0 },
+            {0,0,2,1,0,0,0,0,2,1,0,0,0,0,2,1,0,0,0,0 },
+            {0,0,2,1,0,0,0,0,2,1,0,0,0,0,2,1,0,0,0,0 },
+            {0,0,5,5,4,4,4,4,5,5,4,4,4,4,5,5,4,4,4,4 },
+            {0,0,5,5,3,3,3,3,5,5,3,3,3,3,5,5,3,3,3,3 },
+            {0,0,2,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,2,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+        };//создание дороги
+        int[,] oject = {
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+        };//создание объектов
+        int x_now = -1;//выделение объекта
+        int y_now = -1;//выделение объекта
+        int[] last_cross = new int[2];
+        int[] car = new int[3] { 3, 19, 0 };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 20; i++)//заполение картинками панели
+            {
+                for (int j = 0; j <20; j++)//заполение картинками панели
+                {
+                    PictureBox pic = new PictureBox//заполение картинками панели
+                    {
+                        Name = $"{i}_{j}",//заполение картинками панели
+                        //BorderStyle = BorderStyle.FixedSingle,
+                        Size = new Size(35, 35),//заполение картинками панели
+                        Location = new Point(35 * i, 35 * j),//заполение картинками панели
+                    };
+                    pic.Click += Pic_Click;//выделение и установка объектов
+                    pic.MouseHover += Pic_MouseHover;//отображение кординатов блока
+                    panel1.Controls.Add(pic);//заполение картинками панели
+
+                }
+            }
+            draw();//отрисовка объектов и дороги
+        }
+
+        private void Pic_MouseHover(object sender, EventArgs e)
+        {
+            PictureBox pic = sender as PictureBox;//отображение кординатов блока
+            label1.Text = pic.Name;//отображение кординатов блока
+        }
+
+        private void draw()
+        {
+           for (int i = 0; i < 20; i++)//отрисовка объектов и дороги
+            {
+                for (int j = 0; j < 20; j++)//отрисовка объектов и дороги
+                {
+                    PictureBox pic = panel1.Controls[j + "_" + i] as PictureBox;//отрисовка объектов и дороги
+                    switch (road[i,j])//отрисовка дороги
+                    {
+                        case 1:
+                            pic.BackgroundImage = Properties.Resources.Rvertical;
+                            break;//дорога вверх
+                        case 2:
+                            pic.BackgroundImage = Properties.Resources.Rvertical;
+                            pic.BackgroundImage.RotateFlip(RotateFlipType.RotateNoneFlipXY);
+                            break;//дорога вниз
+                        case 3:
+                            pic.BackgroundImage = Properties.Resources.Rvertical;
+                            pic.BackgroundImage.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                            break;//дорога направо
+                        case 4:
+                            pic.BackgroundImage = Properties.Resources.Rvertical;
+                            pic.BackgroundImage.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                            break;//дорога налево
+                        case 5:
+                            pic.BackgroundImage = Properties.Resources.Rcrossroads;
+                            break;//перекресток
+                        case 6:
+                            pic.BackgroundImage = Properties.Resources.Rstart;
+                            break;//старт
+                        case 7:
+                            pic.BackgroundImage = Properties.Resources.Rfinish;
+                            break;//финиш
+                    }
+                    switch (oject[i, j])
+                    {
+                        case 1:
+                            pic.Image = Properties.Resources.Rblock;
+                            break;//кирпич
+                        case 2:
+                            pic.Image = Properties.Resources.Cvertical1;
+                            break;//машина
+                    }
+                }
+            }
+           
+        } 
+
+        private void Pic_Click(object sender, EventArgs e)
+        {
+            PictureBox pic = sender as PictureBox;//выделение и добавление объектов
+            string[] s = pic.Name.Split('_');//удаление объектов
+            int x = int.Parse(s[1]);//удаление объектов
+            int y = int.Parse(s[0]);//удаление объектов
+            if (x_now > 0)//отчистка рамки
+            {
+                PictureBox pic1 = panel1.Controls[y_now + "_" + x_now] as PictureBox;//отчистка рамки
+                pic1.BorderStyle = BorderStyle.None;//отчистка рамки
+            }
+            button2.Enabled = false;//удаление объектов
+            switch (menu)
+            {
+                case 0:
+                    if (pic.Image != null)
+                    {
+                        pic.BorderStyle = BorderStyle.FixedSingle;
+                        x_now = x;
+                        y_now = y;
+                        button2.Enabled = true;
+                    }
+                    break;//выделение объектов
+                case 1:
+                    if (road[x, y] >= 1 && road[x,y] <=4)
+                    {
+                        pic.Image = Properties.Resources.Rblock;
+                    }
+                    break;//установка кирпича
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            menu = 1;//установка кирпича
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(x_now != -1)//удаление объекта
+            {
+                PictureBox pic = panel1.Controls[y_now + "_" + x_now] as PictureBox;
+                pic.Image = null;
+                x_now = -1;
+                pic.BorderStyle = BorderStyle.None;
+                button2.Enabled = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button4_Click(object sender, EventArgs e)//остановка обучения
+        {
+            timer1.Stop();
+            stop();
+        }
+        private void stop()
+        {
+            PictureBox pic = panel1.Controls[car[0] + "_" + car[1]] as PictureBox;//удаление машины
+            pic.Image = null;//удаление машины
+            car = new int[3] { 3, 19, 0 };//сброс кардинат
+            pic = panel1.Controls[car[0] + "_" + car[1]] as PictureBox;//установка машины
+            pic.Image = Properties.Resources.Cvertical1;//установка машины
+            
+
+            iteration++;
+            label5.Text = iteration.ToString();
+            iteration_time = 0;
+        }
+        private void car_move()
+        {
+            int car_x=0,car_y = 0; 
+            PictureBox pic = panel1.Controls[car[0] + "_" + car[1]] as PictureBox;
+            pic.Image = null;
+            switch (car[2])
+            {
+                case 0:
+                    car_x = car[0];
+                    car_y = car[1]-1;
+                    break;
+                case 1:
+                    car_x = car[0]+1;
+                    car_y = car[1];
+                    break;
+                case 2:
+                    car_x = car[0];
+                    car_y = car[1]+1;
+                    break;
+                case 3:
+                    car_x = car[0]-1;
+                    car_y = car[1];
+                    break;
+            }
+            PictureBox pic1 = panel1.Controls[car_x + "_" + car_y] as PictureBox;
+            if (pic1 != null)
+            {
+                if (pic1.Image == null && road[car_y, car_x] != 6 && road[car_y, car_x] != 0)
+                {
+                    switch (car[2])
+                    {
+                        case 0:
+                            pic1.Image = Properties.Resources.Cvertical1;
+                            break;
+                        case 1:
+                            pic1.Image = Properties.Resources.Cright;
+                            break;
+                        case 2:
+                            pic1.Image = Properties.Resources.Cbottom;
+                            break;
+                        case 3:
+                            pic1.Image = Properties.Resources.Cleft;
+                            break;
+                    }
+                    car[0] = car_x;
+                    car[1] = car_y;
+                    if (road[car_y, car_x] == 5)
+                    {
+                        last_cross[0] = car_x;
+                        last_cross[1] = car_y;
+                    }
+                    if(road[car_y, car_x] == 7)
+                    {
+                        if (iteration_time < best_result)
+                        {
+                            best_result = iteration_time;
+                            label7.Text = best_result.ToString();
+                        }
+                        stop();
+                    }
+                }
+                else
+                {
+                    bool c_t = false;
+                    if (road[car[1], car[0]] == 5)
+                    {
+                        if (pic1.Image != null)
+                        {
+                            road[last_cross[1], last_cross[0]] = 6;
+                            stop();
+                        }
+                        else
+                        {
+                            for (int i = 1; i >=0; i--)
+                            {
+
+                                int nav = turn(i);
+                                switch (nav)
+                                {
+                                    case 0:
+                                        car_x = car[0];
+                                        car_y = car[1] - 1;
+                                        break;
+                                    case 1:
+                                        car_x = car[0] + 1;
+                                        car_y = car[1];
+                                        break;
+                                    case 2:
+                                        car_x = car[0];
+                                        car_y = car[1] + 1;
+                                        break;
+                                    case 3:
+                                        car_x = car[0] - 1;
+                                        car_y = car[1];
+                                        break;
+                                }
+                                if (i == 0)
+                                {
+                                    sw_turn(car[2], ref car_x, ref car_y);
+                                    if (road[car_y, car_x] >= 1 && road[car_y, car_x] <= 5)
+                                    {
+                                        sw_turn(car[2], ref car[0], ref car[1]);
+                                        car[2] = turn(i);
+                                        c_t = true;
+                                        break;
+                                    }
+                                }
+                                else
+                                {
+                                    if (car_x >= 0 && car_x <= 19 && car_y >= 0 && car_y <= 19)
+                                    {
+                                        if (road[car_y, car_x] >= 1 && road[car_y, car_x] <= 5)
+                                        {
+                                            nav = turn(2);
+                                            car_x = car[0];
+                                            car_y = car[1];
+                                            sw_turn(nav, ref car_x, ref car_y);
+                                            if (road[car_y, car_x] < 5)
+                                            {
+                                                car[2] = turn(i);
+                                                c_t = true;
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+
+                                
+                            }
+                            if (!c_t)
+                            {
+                                road[last_cross[1], last_cross[0]] = 6;
+                                stop();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        road[last_cross[1], last_cross[0]] = 6;
+                        stop();
+                    }
+                }
+            }
+            else
+            {
+                road[last_cross[1], last_cross[0]] = 6;
+                stop();
+            } 
+        }
+        private void sw_turn(int sw, ref int x,ref int y)
+        {
+            switch (sw)
+            {
+                case 0:
+                    y--;
+                    break;
+                case 1:
+                    x++;
+                    break;
+                case 2:
+                    y++;
+                    break;
+                case 3:
+                    x--;
+                    break;
+            }
+        }
+        private int turn(int dir)
+        {
+            int nav = car[2];
+            if (dir == 1)
+                nav++;
+            else if (dir == 2)
+                nav += 2;
+            else
+                nav--;
+            if (nav > 3)
+                nav -= 4;
+            else if (nav < 0)
+                nav += 4;
+            return nav;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            timer1.Interval = trackBar1.Value * 100;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            car_move();
+            iteration_time++;
+            label6.Text = iteration_time.ToString();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+    }
+}
